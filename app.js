a474176265ecbb76536ab238b85a2afa4e4610a7
Tehseen Ali <154703743+Tehseenali445@users.noreply.github.com>
2024-03-03 21:19:59 +0500

    Alert Assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+// Q1:
+// alert()
+
+
+// Q2:
+// alert("Error! Please enter a valid password");
+
+// Q3:
+// alert("Welcom to JS Land... \n Happy Coding!" )
+
+// Q4:
+// alert("Welcom to JS land...")
+// alert("Happy Cofing!" )
+
+// Q5:
+
+// alert("Hello... I can run JS throuhg my web browser's console")
+// var message = ("Hello... I can run JS throuhg my web browser's console")
+
+// console.log(message)
+
+// Q6:
+
+// let username = prompt ("Welcome My Portfolio! \n\nEnter Your Full Name")
+
+// console.log(username)
